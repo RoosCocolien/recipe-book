8b19ee063c8969f8656dddd5e4eb65cd51d04bc2
Roos Cocolien <r.steigenga@gmail.com>
2024-04-25 14:53:46 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,26 @@ import { resolve } from 'path'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite'
 
+/**
+ * Some code from the vite documentation for Multi-Page Apps
+ * Debugging is very difficult if we navigate to the about or contact pages
+ * The pages will nog be shown in the devtools
+ * For each page we tell the build tool to resolve its content
+ * We also want to source code to be readable so we want to create a Sourcemap
+ * It is a mapping between the generated/transpiled/minified JavaScript file
+ * and one or more original source files.
+ * This helps debugging. If there is an error, a source map can tell you
+ * the original source file location
+ */
+
 export default defineConfig({
   build: {
     sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'nested/about.html'),
-        contact: resolve(__dirname, 'nested/contact.html')
+        about: resolve(__dirname, 'pages/about.html'),
+        contact: resolve(__dirname, 'pages/contact.html')
       }
     }
   }
